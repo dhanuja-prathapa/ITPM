c92d377cf92c05e741b4139d86dfb1fce13adb98
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-02-17 13:19:15 +0530

    ignore

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
-echo "Hello World";
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WE_43</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<form action="/action_page.php" id="form1">
+    <label for="codetitle">Paste the code:</label>
+    <h2>Code Here</h2>
+    <textarea name="code" cols="100" rows="10"></textarea><br><br>
+    <input type="submit" class="btn btn-primary" value="Analyse">
+</form>
+</body>
+</html>
