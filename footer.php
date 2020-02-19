7c2d3c7a8a9a2e211004a002a05853e64003e234
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-02-19 19:49:49 +0530

    footer font

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer  navbar-dark bg-dark box-shadow">
     <div class="container">
-        <span>Developed by Dhanuj Ranawake, Savandi Bandaranayake, Malith Lahiru.</span>
-        <span style="position: fixed;right: 20%;"> © 2020 <a href="https://www.marusira.co/">Marusira</a></span>
+        <span>Developed by Dhanuja Ranawake, Savandi Bandaranayake, Malith Lahiru.</span>
+        <span class="marusira hidden-xs"> © 2020 <a href="https://www.marusira.co/">Marusira</a></span>
 
     </div>
 </footer>
