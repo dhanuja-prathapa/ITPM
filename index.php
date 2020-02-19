06b26e1785068b055fe4deacc82fb09ff64741dc
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-02-19 06:54:50 +0530

    css refine

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
         </div>
     </div>
 </header>
-<main style="margin-left: 27%"><br><br>
-<form action="/action_page.php" id="form1" method="post">
+<main style="margin-left: 27%;margin-right: 27%;"><br><br>
+<form action="/action_page.php" id="form1" method="post" class="rounded" style="padding: 4%;background-color: #f5f5f5">
     <h2>Paste the Code Here</h2>
     <div class="form-group">
-    <textarea class="form-control border border-secondary" name="code" style="width: 73%" rows="20"></textarea><br><br>
+    <textarea class="form-control border border-secondary rounded" name="code" style="width: 100%" rows="20"></textarea><br>
     </div>
     <input type="submit" class="btn btn-primary" value="Analyse">
     <button type="reset" class="btn btn-secondary">Clear</button>
