7f101040a168c0400d6a8f9f6999e9a89a0a71c4
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-02-25 12:20:16 +0530

    format the php

--- action_page.php
+++ action_page.php
@@ -1,103 +1,102 @@
 <?php include "header.php"; ?>
 <main>
-<!-- Dhanuja Ranawake part -->
+    <!-- Dhanuja Ranawake part -->
     <br><br>
     <div style="margin-left: 20%;margin-right:20%">
-    <h5 style="text-align: center;"> Displaying the complexity of a program due to size</h5><br>
-<?php
-require 'codeSVM.php';
-require 'controlStructures.php';
-//Getting the code
-$code = $_POST['code'];
-//separating the lines
-$codes = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $code);
-$codes = explode("\n",$codes);
+        <h5 style="text-align: center;"> Displaying the complexity of a program due to size</h5><br>
+        <?php
+        require 'codeSVM.php';
+        require 'controlStructures.php';
+        //Getting the code
+        $code = $_POST['code'];
+        //separating the lines
+        $codes = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $code);
+        $codes = explode("\n", $codes);
 
-//define arrays
-$cs = array_fill(1,sizeof($codes),0);
-$nkw = array_fill(1,sizeof($codes),0);
-$nid = array_fill(1,sizeof($codes),0);
-$nop = array_fill(1,sizeof($codes),0);
-$nnv = array_fill(1,sizeof($codes),0);
-$nsl = array_fill(1,sizeof($codes),0);
+        //define arrays
+        $cs = array_fill(1, sizeof($codes), 0);
+        $nkw = array_fill(1, sizeof($codes), 0);
+        $nid = array_fill(1, sizeof($codes), 0);
+        $nop = array_fill(1, sizeof($codes), 0);
+        $nnv = array_fill(1, sizeof($codes), 0);
+        $nsl = array_fill(1, sizeof($codes), 0);
 
-//define arrays for control structures
-$ccs = array_fill(1,sizeof($codes),0);
-$wtcs = array_fill(1,sizeof($codes),0);
-$nc = array_fill(1,sizeof($codes),0);
-$ccspps = array_fill(1,sizeof($codes),0);
+        //define arrays for control structures
+        $ccs = array_fill(1, sizeof($codes), 0);
+        $wtcs = array_fill(1, sizeof($codes), 0);
+        $nc = array_fill(1, sizeof($codes), 0);
+        $ccspps = array_fill(1, sizeof($codes), 0);
 
-//Analysis
-sizeCal($codes);
-calCs();
-//Open the table and its first row
-echo "<table class=\"table table-bordered table-striped \">";
-echo "<thead class=\"thead-dark\">";
-echo "<tr>";
-echo "<th style='width: 7%' scope=\"col\">Line no</th>";
-echo "<th scope=\"col\">Program statements</th>";
-echo "<th scope=\"col\">Nkw</th>";
-echo "<th scope=\"col\">Nid</th>";
-echo "<th scope=\"col\">Nop</th>";
-echo "<th scope=\"col\">Nnv</th>";
-echo "<th scope=\"col\">Nsl</th>";
-echo "<th scope=\"col\">Cs</th>";
-echo "</tr>";
-echo "</thead>";
+        //Analysis
+        sizeCal($codes);
+        calCs();
+        //Open the table and its first row
+        echo "<table class=\"table table-bordered table-striped \">";
+        echo "<thead class=\"thead-dark\">";
+        echo "<tr>";
+        echo "<th style='width: 7%' scope=\"col\">Line no</th>";
+        echo "<th scope=\"col\">Program statements</th>";
+        echo "<th scope=\"col\">Nkw</th>";
+        echo "<th scope=\"col\">Nid</th>";
+        echo "<th scope=\"col\">Nop</th>";
+        echo "<th scope=\"col\">Nnv</th>";
+        echo "<th scope=\"col\">Nsl</th>";
+        echo "<th scope=\"col\">Cs</th>";
+        echo "</tr>";
+        echo "</thead>";
 
-//Add empty <td>'s to even up the amount of cells in a row:
-$lineno = 1;
-foreach ($codes as $line){
-    echo "<tr>";
-    echo "<th scope= \"row\">$lineno</th>";
-    echo "<td>$line</td>";
-    echo "<td>$nkw[$lineno]</td>";
-    echo "<td>$nid[$lineno]</td>";
-    echo "<td>$nop[$lineno]</td>";
-    echo "<td>$nnv[$lineno]</td>";
-    echo "<td>$nsl[$lineno]</td>";
-    echo "<td>$cs[$lineno]</td>";
-    echo "</tr>";
-    $lineno++;
-}
+        //Add empty <td>'s to even up the amount of cells in a row:
+        $lineno = 1;
+        foreach ($codes as $line) {
+            echo "<tr>";
+            echo "<th scope= \"row\">$lineno</th>";
+            echo "<td>$line</td>";
+            echo "<td>$nkw[$lineno]</td>";
+            echo "<td>$nid[$lineno]</td>";
+            echo "<td>$nop[$lineno]</td>";
+            echo "<td>$nnv[$lineno]</td>";
+            echo "<td>$nsl[$lineno]</td>";
+            echo "<td>$cs[$lineno]</td>";
+            echo "</tr>";
+            $lineno++;
+        }
 
-//Close the table row and the table
-echo "</table><br>";
+        //Close the table row and the table
+        echo "</table><br>";
 
-//heading for control structure table
-echo "<h5 style=\"text-align: center;\"> Displaying the complexity of a program due to control structures</h5><br>
+        //heading for control structure table
+        echo "<h5 style=\"text-align: center;\"> Displaying the complexity of a program due to control structures</h5><br>
 ";
 
-findForLoops($codes);
-//defining columns for the control structure table
-echo "<table class=\"table table-bordered table-striped \">";
-echo "<thead class=\"thead-dark\">";
-echo "<tr>";
-echo "<th style='width: 7%' scope=\"col\">Line no</th>";
-echo "<th scope=\"col\">Program statements</th>";
-echo "<th scope=\"col\">Wtcs</th>";
-echo "<th scope=\"col\">NC</th>";
-echo "<th scope=\"col\">Ccspps</th>";
-echo "<th scope=\"col\">Ccs</th>";
-echo "</tr>";
-echo "</thead>";
+        findForLoops($codes);
+        //defining columns for the control structure table
+        echo "<table class=\"table table-bordered table-striped \">";
+        echo "<thead class=\"thead-dark\">";
+        echo "<tr>";
+        echo "<th style='width: 7%' scope=\"col\">Line no</th>";
+        echo "<th scope=\"col\">Program statements</th>";
+        echo "<th scope=\"col\">Wtcs</th>";
+        echo "<th scope=\"col\">NC</th>";
+        echo "<th scope=\"col\">Ccspps</th>";
+        echo "<th scope=\"col\">Ccs</th>";
+        echo "</tr>";
+        echo "</thead>";
 
-$lineno = 1;
-foreach ($codes as $line){
-    echo "<tr>";
-    echo "<th scope= \"row\">$lineno</th>";
-    echo "<td>$line</td>";
-    echo "<td>$wtcs[$lineno]</td>";
-    echo "<td>$nc[$lineno]</td>";
-    echo "<td>$ccspps[$lineno]</td>";
-    echo "<td>$ccs[$lineno]</td>";
-    echo "</tr>";
-    $lineno++;
+        $lineno = 1;
+        foreach ($codes as $line) {
+            echo "<tr>";
+            echo "<th scope= \"row\">$lineno</th>";
+            echo "<td>$line</td>";
+            echo "<td>$wtcs[$lineno]</td>";
+            echo "<td>$nc[$lineno]</td>";
+            echo "<td>$ccspps[$lineno]</td>";
+            echo "<td>$ccs[$lineno]</td>";
+            echo "</tr>";
+            $lineno++;
 
-    echo "</table><br>";
-
-}
-?></div>
-
+            echo "</table><br>";
+        }
+        ?>
+    </div>
 </main>
 <?php include "footer.php"; ?>
--- codeSVM.php
+++ codeSVM.php
@@ -1,62 +1,69 @@
 <?php
-function numericalVal($lines){
+function numericalVal($lines)
+{
     return (preg_match_all('!\d+!', $lines));
-    
 }
 
-function calCs(){
-    global $nkw,$nop,$nid,$nnv,$nsl,$cs;
-    for ($i = 1; $i <= sizeof($cs); $i++){
+function calCs()
+{
+    global $nkw, $nop, $nid, $nnv, $nsl, $cs;
+    for ($i = 1; $i <= sizeof($cs); $i++) {
         $cs[$i] = $nkw[$i] + $nid[$i] + $nop[$i] + $nnv[$i] + $nsl[$i];
     }
 }
 
-function stringLiterals($lines){
-    if (substr_count($lines,'"') != 0){
+function stringLiterals($lines)
+{
+    if (substr_count($lines, '"') != 0) {
         $counts = preg_match_all('/"/', $lines);
-        return $counts/2;
-    }else {return 0;}
+        return $counts / 2;
+    } else {
+        return 0;
+    }
 }
 
-function findNid($lines,$linesno){
-    if (substr_count($lines,"System.out.") != 0){
-       
-        if ($nidcount = (substr_count($lines,"+") + substr_count($lines,"-") + substr_count($lines,"/") + substr_count($lines,"*"))!= 0){
+function findNid($lines, $linesno)
+{
+    if (substr_count($lines, "System.out.") != 0) {
+
+        if ($nidcount = (substr_count($lines, "+") + substr_count($lines, "-") + substr_count($lines, "/") + substr_count($lines, "*")) != 0) {
             global $nid;
             $nid[$linesno] += $nidcount;
         }
     }
 }
 
-function subOpCount($word,$linesno){
-    if ($count = substr_count($word,"System.out.") != 0 ){
+function subOpCount($word, $linesno)
+{
+    if ($count = substr_count($word, "System.out.") != 0) {
         global $nid;
         $nid[$linesno] += 3;
         return ($count * 2);
     }
-    if ($count = (substr_count($word,"++") + substr_count($word,"--")) != 0){
+    if ($count = (substr_count($word, "++") + substr_count($word, "--")) != 0) {
         return $count;
+    } else {
+        return 0;
     }
-    else {return 0;}
 }
 
-function sizeCal ($codes)
+function sizeCal($codes)
 {
-    global $nkw,$nop,$nid,$nnv,$nsl;
+    global $nkw, $nop, $nid, $nnv, $nsl;
     $linesno = 1;
     foreach ($codes as $lines) {
         $words = explode(" ", $lines);
         foreach ($words as $word) {
             $string_json = file_get_contents("javaKey.json");
-            $pattern = json_decode($string_json,TRUE);
-            foreach ($pattern as $i){
-            if($count = preg_match_all($i,$word) != 0){
-                $nkw[$linesno] += $count;
-            }
+            $pattern = json_decode($string_json, TRUE);
+            foreach ($pattern as $i) {
+                if ($count = preg_match_all($i, $word) != 0) {
+                    $nkw[$linesno] += $count;
+                }
             }
             switch ($word) {
-                //keywords
-               
+                    //keywords
+
                 case 'static':
                 case 'class':
                 case 'interface':
@@ -64,7 +71,7 @@ function sizeCal ($codes)
                     $nid[$linesno]++;
                     break;
 
-                //operators
+                    //operators
                 case '>':
                 case '>=':
                 case '<':
@@ -110,11 +117,10 @@ function sizeCal ($codes)
                 default:
                     $nop[$linesno] += subOpCount($word, $linesno);
             }
-
         }
         $nsl[$linesno] += stringLiterals($lines);
         $nnv[$linesno] += numericalVal($lines);
-        findNid($lines,$linesno);
+        findNid($lines, $linesno);
         $linesno++;
     }
 }
