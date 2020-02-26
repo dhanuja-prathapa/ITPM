1adacb9d7e42cfc60c6120e0c82659e947487b0c
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-02-26 14:40:21 +0530

    upload js

--- header.php
+++ header.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <link rel='icon' href='favicon.ico' type='image/x-icon'/ >
+    <link rel='icon' href='favicon.ico' type='image/x-icon'/>
     <title>ITPM WE-45</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/jquery-3.1.1.slim.min.js"></script>
+    <script src="js/tether.min.js"></script>
 </head>
+
 <body>
-<header>
-    <div class="navbar navbar-dark bg-dark box-shadow">
-        <div class="container d-flex justify-content-between">
-            <a href="/ITPM" class="navbar-brand d-flex align-items-center">
-                <strong>IT Project Management</strong>
-            </a>
+    <header>
+        <div class="navbar navbar-dark bg-dark box-shadow">
+            <div class="container d-flex justify-content-between">
+                <a href="/ITPM" class="navbar-brand d-flex align-items-center">
+                    <strong>IT Project Management</strong>
+                </a>
+            </div>
         </div>
-    </div>
-</header>
+    </header>
--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php include "header.php"; ?>
 <main style="margin-left: 20%;margin-right: 20%;"><br><br>
-<form action="action_page.php" id="form1" method="post" class="rounded" style="padding: 4%;background-color: #f5f5f5">
+  <form action="action_page.php" id="form1" method="post" class="rounded" style="padding: 4%;background-color: #f5f5f5">
     <h2>Paste the Code Here</h2>
     <div class="form-group">
-    <textarea class="form-control border border-secondary rounded" name="code" style="width: 100%" rows="15"></textarea><br>
+      <textarea class="form-control rounded" id="sourceinput" name="code" style="width: 100%; border:dashed" rows="15"></textarea><br>
     </div>
-    <div class="box__input">
-    <input class="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
-    <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
-    <button class="box__button" type="submit">Upload</button>
-  </div>
-  <div class="box__uploading">Uploading&hellip;</div>
-  <div class="box__success">Done!</div>
-  <div class="box__error">Error! <span></span>.</div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Upload</span>
+      </div>
+      <div class="custom-file">
+        <input type="file" class="custom-file-input" id="inputGroupFile01">
+        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+      </div>
+    </div>
+    <script>
+      $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+      });
+    </script>
     <input type="submit" class="btn btn-primary" value="Analyse">
     <button type="reset" class="btn btn-secondary">Clear</button>
-</form>
+  </form>
 </main><br><br>
 <?php include "footer.php"; ?>
 </body>
+
 </html>
