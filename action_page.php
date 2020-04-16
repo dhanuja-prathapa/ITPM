9d93f95d8ce0fad87afc15c63efe2ef614b2de4c
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-04-16 19:04:25 +0530

    fileupload

--- action_page.php
+++ action_page.php
@@ -9,101 +9,119 @@
         require 'codeSVM.php';
         require 'controlStructures.php';
 
-        //Getting the code
-        $code = $_POST['code'];
+        //file upload
 
-        //separating the lines
-        $codes = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $code);
-        $codes = explode("\n", $codes);
-
-        //define arrays
-        $cs = array_fill(1, sizeof($codes), 0);
-        $nkw = array_fill(1, sizeof($codes), 0);
-        $nid = array_fill(1, sizeof($codes), 0);
-        $nop = array_fill(1, sizeof($codes), 0);
-        $nnv = array_fill(1, sizeof($codes), 0);
-        $nsl = array_fill(1, sizeof($codes), 0);
-
-        //define arrays for control structures
-        $ccs = array_fill(1, sizeof($codes), 0);
-        $wtcs = array_fill(1, sizeof($codes), 0);
-        $nc = array_fill(1, sizeof($codes), 0);
-        $ccspps = array_fill(1, sizeof($codes), 0);
-
-        $TABLE_START = "<tr>";
-        $TABLE_END = "</tr>";
-
-        //Analysis
-        sizeCal($codes);
-        calCs();
-
-        //Open the table and its first row
-        echo "<table class=\"table table-bordered table-striped \">";
-        echo "<thead class=\"thead-dark\">";
-        echo $TABLE_START;
-        echo "<th style='width: 7%' scope=\"col\">Line no</th>";
-        echo "<th scope=\"col\">Program statements</th>";
-        echo "<th scope=\"col\">Nkw</th>";
-        echo "<th scope=\"col\">Nid</th>";
-        echo "<th scope=\"col\">Nop</th>";
-        echo "<th scope=\"col\">Nnv</th>";
-        echo "<th scope=\"col\">Nsl</th>";
-        echo "<th scope=\"col\">Cs</th>";
-        echo $TABLE_END;
-        echo "</thead>";
-
-        //Add empty <td>'s to even up the amount of cells in a row:
-        $lineno = 1;
-        foreach ($codes as $line) {
-            echo $TABLE_START;
-            echo "<th scope= \"row\">$lineno</th>";
-            echo "<td>$line</td>";
-            echo "<td>$nkw[$lineno]</td>";
-            echo "<td>$nid[$lineno]</td>";
-            echo "<td>$nop[$lineno]</td>";
-            echo "<td>$nnv[$lineno]</td>";
-            echo "<td>$nsl[$lineno]</td>";
-            echo "<td>$cs[$lineno]</td>";
-            echo $TABLE_END;
-            $lineno++;
+        // Check if image file is a actual image or fake image
+        if(isset($_POST['submit'])){
+            $name       = $_FILES['file']['name'];
+            $temp_name  = $_FILES['file']['tmp_name'];
+            if(isset($name) and !empty($name)){
+                print_r($_FILES['file']);
+                $location = 'uploads/';
+                if(move_uploaded_file($temp_name, $location.$name)){
+                    echo 'File uploaded successfully';
+                }
+            } else {
+                echo 'You should select a file to upload !!';
+            }
         }
 
-        //Close the table row and the table
-        echo "</table><br>";
-
-        //heading for control structure table
-        echo "<h5 style=\"text-align: center;\"> Displaying the complexity of a program due to control structures</h5><br>";
-
-        findForLoops($codes);
-        calCcs();
-
-        //defining columns for the control structure table
-        echo "<table class=\"table table-bordered table-striped \">";
-        echo "<thead class=\"thead-dark\">";
-        echo $TABLE_START;
-        echo "<th style='width: 7%' scope=\"col\">Line no</th>";
-        echo "<th scope=\"col\">Program statements</th>";
-        echo "<th scope=\"col\">Wtcs</th>";
-        echo "<th scope=\"col\">NC</th>";
-        echo "<th scope=\"col\">Ccspps</th>";
-        echo "<th scope=\"col\">Ccs</th>";
-        echo $TABLE_END;
-        echo "</thead>";
-
-        $lineno = 1;
-        foreach ($codes as $line) {
-            echo $TABLE_START;
-            echo "<th scope= \"row\">$lineno</th>";
-            echo "<td>$line</td>";
-            echo "<td>$wtcs[$lineno]</td>";
-            echo "<td>$nc[$lineno]</td>";
-            echo "<td>$ccspps[$lineno]</td>";
-            echo "<td>$ccs[$lineno]</td>";
-            echo $TABLE_END;
-            $lineno++;
-        }
-        echo "</table><br>";
-        ?>
+//
+//        //Getting the code
+//        $code = $_POST['code'];
+//
+//        //separating the lines
+//        $codes = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $code);
+//        $codes = explode("\n", $codes);
+//
+//        //define arrays
+//        $cs = array_fill(1, sizeof($codes), 0);
+//        $nkw = array_fill(1, sizeof($codes), 0);
+//        $nid = array_fill(1, sizeof($codes), 0);
+//        $nop = array_fill(1, sizeof($codes), 0);
+//        $nnv = array_fill(1, sizeof($codes), 0);
+//        $nsl = array_fill(1, sizeof($codes), 0);
+//
+//        //define arrays for control structures
+//        $ccs = array_fill(1, sizeof($codes), 0);
+//        $wtcs = array_fill(1, sizeof($codes), 0);
+//        $nc = array_fill(1, sizeof($codes), 0);
+//        $ccspps = array_fill(1, sizeof($codes), 0);
+//
+//        $TABLE_START = "<tr>";
+//        $TABLE_END = "</tr>";
+//
+//        //Analysis
+//        sizeCal($codes);
+//        calCs();
+//
+//        //Open the table and its first row
+//        echo "<table class=\"table table-bordered table-striped \">";
+//        echo "<thead class=\"thead-dark\">";
+//        echo $TABLE_START;
+//        echo "<th style='width: 7%' scope=\"col\">Line no</th>";
+//        echo "<th scope=\"col\">Program statements</th>";
+//        echo "<th scope=\"col\">Nkw</th>";
+//        echo "<th scope=\"col\">Nid</th>";
+//        echo "<th scope=\"col\">Nop</th>";
+//        echo "<th scope=\"col\">Nnv</th>";
+//        echo "<th scope=\"col\">Nsl</th>";
+//        echo "<th scope=\"col\">Cs</th>";
+//        echo $TABLE_END;
+//        echo "</thead>";
+//
+//        //Add empty <td>'s to even up the amount of cells in a row:
+//        $lineno = 1;
+//        foreach ($codes as $line) {
+//            echo $TABLE_START;
+//            echo "<th scope= \"row\">$lineno</th>";
+//            echo "<td>$line</td>";
+//            echo "<td>$nkw[$lineno]</td>";
+//            echo "<td>$nid[$lineno]</td>";
+//            echo "<td>$nop[$lineno]</td>";
+//            echo "<td>$nnv[$lineno]</td>";
+//            echo "<td>$nsl[$lineno]</td>";
+//            echo "<td>$cs[$lineno]</td>";
+//            echo $TABLE_END;
+//            $lineno++;
+//        }
+//
+//        //Close the table row and the table
+//        echo "</table><br>";
+//
+//        //heading for control structure table
+//        echo "<h5 style=\"text-align: center;\"> Displaying the complexity of a program due to control structures</h5><br>";
+//
+//        findForLoops($codes);
+//        calCcs();
+//
+//        //defining columns for the control structure table
+//        echo "<table class=\"table table-bordered table-striped \">";
+//        echo "<thead class=\"thead-dark\">";
+//        echo $TABLE_START;
+//        echo "<th style='width: 7%' scope=\"col\">Line no</th>";
+//        echo "<th scope=\"col\">Program statements</th>";
+//        echo "<th scope=\"col\">Wtcs</th>";
+//        echo "<th scope=\"col\">NC</th>";
+//        echo "<th scope=\"col\">Ccspps</th>";
+//        echo "<th scope=\"col\">Ccs</th>";
+//        echo $TABLE_END;
+//        echo "</thead>";
+//
+//        $lineno = 1;
+//        foreach ($codes as $line) {
+//            echo $TABLE_START;
+//            echo "<th scope= \"row\">$lineno</th>";
+//            echo "<td>$line</td>";
+//            echo "<td>$wtcs[$lineno]</td>";
+//            echo "<td>$nc[$lineno]</td>";
+//            echo "<td>$ccspps[$lineno]</td>";
+//            echo "<td>$ccs[$lineno]</td>";
+//            echo $TABLE_END;
+//            $lineno++;
+//        }
+//        echo "</table><br>";
+//        ?>
     </div>
 </main>
 <?php include "footer.php"; ?>
--- index.php
+++ index.php
@@ -2,17 +2,13 @@
 
 <body>
 <main style="margin-left: 20%;margin-right: 20%;"><br><br>
-  <form action="action_page.php" id="form1" method="post" class="rounded" style="padding: 4%;background-color: #f5f5f5">
-    <h2>Paste the Code Here</h2>
-    <div class="form-group">
-      <textarea class="form-control rounded" id="sourceinput" name="code" style="width: 100%; border:dashed" rows="15"></textarea><br>
-    </div>
+  <form action="action_page.php" id="form1" method="post" enctype="multipart/form-data" class="rounded" style="padding: 4%;background-color: #f5f5f5">
     <div class="input-group mb-3">
       <div class="input-group-prepend">
         <span class="input-group-text">Upload</span>
       </div>
       <div class="custom-file">
-        <input type="file" class="custom-file-input" id="inputGroupFile01">
+        <input type="file" class="custom-file-input" name="file" id="file">
         <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
       </div>
     </div>
@@ -22,7 +18,7 @@
         $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
       });
     </script>
-    <input type="submit" class="btn btn-primary" value="Analyse">
+    <input type="submit" name="submit" class="btn btn-primary" value="Analyse">
     <button type="reset" class="btn btn-secondary">Clear</button>
   </form>
 </main><br><br>
