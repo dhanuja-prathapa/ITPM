ecf880e8e9915cef81728ae92d3e3282feb7a1bd
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-04-18 11:09:11 +0530

    uploads folder created

--- action_page.php
+++ action_page.php
@@ -18,6 +18,7 @@
             if(isset($name) and !empty($name)){
                 $location = 'uploads/';
                 if(move_uploaded_file($temp_name, $location.$name)){
+                    echo 'File uploaded successfully';
                 }
             } else {
                 echo 'You should select a file to upload !!';
